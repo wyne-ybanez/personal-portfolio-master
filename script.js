dc9e8f2beb89b77f27dd488ea75ed3c50bf09a24
wyne-ybanez <wyne_01@icloud.com>
2021-06-25 17:43:37 +0000

    Add email js functionality, delete thankyou.html file, add grey background to project descriptions

--- script.js
+++ script.js
@@ -1,16 +1,55 @@
-// Transparent Scroll Effect of Navbar from: https://stackoverflow.com/questions/29646622/set-bootstrap-navbar-transparency-on-scroll
-function checkScroll(){
-    var startY = $('.navbar').height() * 2; 
+$(document).ready(function () {
 
-    if($(window).scrollTop() > startY){
-        $('.navbar').addClass("scrolled");
-    }else{
-        $('.navbar').removeClass("scrolled");
+    // Send Email to Developer
+    function sendMail(){
+        phone_number =  $('#phone').val()
+        from_name = $('#fname').val()
+        from_email = $('#emailaddress').val()
+        message = $('#commentarea').val()
+
+        inputs = [phone_number,from_name,from_email,message]
+
+        emailjs.send("WyneYbanez","template_huak0pk", {
+            phone_number:  phone_number,
+            from_name: from_name,
+            from_email: from_email,
+            message: message,
+          })
+          .then(
+            function (response) {
+              window.alert("Message Sent 👍");
+              window.location.reload();
+              console.log('SUCCESS', response)
+            },
+            function (error) {
+              window.alert("Message failed to send 😔");
+              console.log('FAILED', error)
+            }
+          )
+        return false  
     }
-}
 
-if($('.navbar').length > 0){
-    $(window).on("scroll load resize", function(){
-        checkScroll();
-    });
-}
+    $("#Message-Form").on('submit', function() {
+        sendMail();
+    })
+
+    // Transparent Scroll Effect of Navbar from: https://stackoverflow.com/questions/29646622/set-bootstrap-navbar-transparency-on-scroll
+    function checkScroll(){
+        var startY = $('.navbar').height() * 2; 
+
+        if($(window).scrollTop() > startY){
+            $('.navbar').addClass("scrolled");
+        }else{
+            $('.navbar').removeClass("scrolled");
+        }
+    }
+        if($('.navbar').length > 0){
+            $(window).on("scroll load resize", function(){
+                checkScroll();
+            });
+        }
+})
+
+
+
+
